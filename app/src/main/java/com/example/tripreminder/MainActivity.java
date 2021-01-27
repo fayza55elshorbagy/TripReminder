3e3cabd8eddd2e2d90aa988cc9050c654946aa75
fayza55elshorbagy <47149903+fayza55elshorbagy@users.noreply.github.com>
2021-01-27 21:59:21 +0200

    connectingFirebase
    firebaseRead_Write

--- app/src/main/java/com/example/tripreminder/MainActivity.java
+++ app/src/main/java/com/example/tripreminder/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.tripreminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Data");
+    }
+
+    public void save(View view) {
+        String WritedName = name.getText().toString();
+        databaseReference.setValue(WritedName);
+    }
+
+    public void read(View view) {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String ReadedName = snapshot.getValue().toString();
+                    Toast.makeText(MainActivity.this, ReadedName, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
